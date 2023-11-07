7858719948431d7c8cc284c57ac80211405d7db2
nimysan <234aini@gmail.com>
2023-11-07 19:25:49 +0800

    initialize

---
+++ app/src/main/java/red/plaza/devicefarmplay/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package red.plaza.devicefarmplay.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
